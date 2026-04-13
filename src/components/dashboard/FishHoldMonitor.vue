a675760d4d9529af328d6c6e9ee110da0caca119
Yeji 20181292 <se84838483@gmail.com>
2026-04-13 15:15:51 +0900

    fix: 2,3번 P창고 수정1

--- src/components/dashboard/FishHoldMonitor.vue
+++ src/components/dashboard/FishHoldMonitor.vue
@@ -25,11 +25,11 @@ const fishHolds = ref<FishHold[]>([
   { id: '6p', name: 'No.6 (P)', temp: -2.2, top: '39%', left: '44%', width: '6%', height: '5%' },
   { id: '5p', name: 'No.5 (P)', temp: -3.6, top: '39%', left: '53%', width: '6%', height: '5%' },
   { id: '4p', name: 'No.4 (P)', temp: -1.5, top: '39%', left: '62%', width: '6%', height: '5%' },
-  { id: '3p', name: 'No.3 (P)', temp: -0.1, top: '39%', left: '71%', width: '6%', height: '5%' },
-  { id: '2p', name: 'No.2 (P)', temp: -10.2, top: '39%', left: '80%', width: '6%', height: '5%' },
+  { id: '3p', name: 'No.3 (P)', temp: -0.1, top: '45%', left: '71%', width: '6%', height: '5%' },
+  { id: '2p', name: 'No.2 (P)', temp: -10.2, top: '45%', left: '80%', width: '6%', height: '5%' },
 
   // Bow
-  { id: '1', name: 'No.1', temp: 20.5, top: '55%', left: '88.5%', width: '5%', height: '5%' },
+  { id: '1', name: 'No.1', temp: 20.5, top: '54.5%', left: '88.5%', width: '5%', height: '5%' },
 
   // Starboard (S) Side - Bottom Row (from stern to bow)
   { id: '10s', name: 'No.10 (S)', temp: -11.1, top: '69%', left: '8%', width: '6%', height: '5%' },
