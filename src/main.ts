5b95cf7fea7094a1b5fa948dda658f3fef085584
Yeji 20181292 <se84838483@gmail.com>
2026-04-17 09:06:53 +0900

    fix: 구글api 클라이언트 id 발급

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import App from './App.vue'
 const app = createApp(App)
 
 app.use(vue3GoogleLogin, {
-  clientId: 'YOUR_GOOGLE_CLIENT_ID_HERE' // 사용자분께서 발급받은 실제 Client ID로 변경해주세요
+  clientId: '243107023418-eieu5qbiu81sht5r7f1sauvsij8n8e0k.apps.googleusercontent.com'
 })
 
 app.mount('#app')
